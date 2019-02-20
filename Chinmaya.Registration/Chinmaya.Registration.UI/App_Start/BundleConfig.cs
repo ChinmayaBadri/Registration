fa77e9c089e04020f257db8388a6d8fdbf2e32c6
rakesh <rakesh@CESL-HF-D-094>
2019-02-20 17:13:15 +0530

    Updated Nuget Packages & Connection String

--- Chinmaya.Registration/Chinmaya.Registration.UI/App_Start/BundleConfig.cs
+++ Chinmaya.Registration/Chinmaya.Registration.UI/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace Chinmaya.Registration.UI
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new Bundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-3.3.1.js",
                         "~/Scripts/popper.min.js",
                         "~/Scripts/Toastr/toastr.min.js",
@@ -16,22 +16,22 @@ namespace Chinmaya.Registration.UI
 						"~/Scripts/DataTables/jquery.dataTables.js",
                         "~/Scripts/jquery-confirm.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            bundles.Add(new Bundle("~/bundles/jqueryval").Include(
                         "~/Scripts/Validate/jquery.validate.js",
                         "~/Scripts/Validate/jquery.validate.unobtrusive.js",
                         "~/Scripts/jquery.unobtrusive-ajax.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            bundles.Add(new Bundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.min.js",
                       "~/Scripts/bootstrap-datepicker.min.js",
                       "~/Scripts/Chinmaya/Common.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new Bundle("~/Content/css").Include(
                       "~/Content/bootstrap.min.css",
                       "~/Content/bootstrap-datepicker.standalone.min.css",
                       "~/Content/custom-styles.css",
